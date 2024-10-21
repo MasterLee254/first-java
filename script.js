1d8050f6c84ab34db767086bcfbdfb2f392bc26d
MasterLee254 <masterlee.mmn@gmail.com>
2024-10-21 14:38:05 +0300

    add code to prompt user choice input

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ function getComputerChoice() {
     //computerChoice function that will return either rock, paper or scissors
 }
 
-console.log(getComputerChoice())
+console.log(`Computer chose: ${getComputerChoice()}`)
+
+// Function to get user choice
+function getUserChoice() {
+    let userInput = prompt("Enter your choice (rock, paper, scissors):").toLowerCase();
+
+    // Validate input
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        console.log(`You chose: ${userInput}`);
+        return userInput; // Return valid choice
+    } else {
+        console.log("Invalid choice! Please enter rock, paper, or scissors.");
+        return getUserChoice(); // Ask again if input is invalid
+    }
+}
+
+console.log(getUserChoice())
