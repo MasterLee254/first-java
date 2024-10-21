1978ffe7da26addef68cd42b9e402c27a0cface3
MasterLee254 <masterlee.mmn@gmail.com>
2024-10-21 12:36:07 +0300

    write a omputerChoice function that will return either rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+/* script.js */
 
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+    //computerChoice function that will return either rock, paper or scissors
+}
+
+console.log(getComputerChoice())
